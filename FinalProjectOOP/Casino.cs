ecb28563d99498c22bb2da6cc400547902e00c3b
Jonathan <jonathan.owen@students.snow.edu>
2025-04-01 14:47:58 -0600

    Add project files.

---
+++ FinalProjectOOP/Casino.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProjectOOP
+{
+    public class Casino
+    {
+        private string Name { get; }
+        public Casino(string name) 
+        {
+            Name = name;
+        }
+
+    }
+}
