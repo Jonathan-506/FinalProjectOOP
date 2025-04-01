aea30c9c17e37f7be8f4ce3d7f59775ddd941e48
jonathan.owen <jonathan.owen@students.snow.edu>
2025-04-01 15:05:17 -0600

    Added Character Class

---
+++ FinalProjectOOP/Character.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProjectOOP
+{
+    class Character
+    {
+        public string Name { get; set; }
+
+        public int Chips { get; set; }
+
+        public Character()
+        {
+
+        }
+    }
+
+    class Player : Character
+    {
+        public Player()
+        {
+
+        }
+
+
+    }
+
+    class NPC : Character
+    {
+
+    }
+}
