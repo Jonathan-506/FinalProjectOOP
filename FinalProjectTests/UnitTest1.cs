84007e5919a1468da8383b7283c1eb04fed195e3
jonathan.owen <jonathan.owen@students.snow.edu>
2025-04-08 14:34:34 -0600

    Added NUnit Test project

---
+++ FinalProjectTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+using FinalProjectOOP;
+
+namespace FinalProjectTests
+{
+    public class Tests
+    {
+        public Deck deck { get; set; } 
+
+        [SetUp]
+        public void Setup()
+        {
+            deck = new Deck();
+        }
+
+        [Test]
+        public void CreateDeckTest()
+        {
+
+
+            
+            Assert.AreEqual(52, deck.Cards.Count);
+        }
+    }
+}
